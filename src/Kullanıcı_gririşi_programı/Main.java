a05ab23b88111306bb81e2d977733757dd7c0bb3
crossgate67 <93184472+crossgate67@users.noreply.github.com>
2022-04-01 17:00:19 +0300

    update 5.5

--- src/Kullanıcı_gririşi_programı/Main.java
+++ src/Kullanıcı_gririşi_programı/Main.java
@@ -2,17 +2,17 @@ package Kullanıcı_gririşi_programı;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        String username = "ceyhun",password = "dippper46",new_password;
+        String username,password,new_password;
         String secim;
         Scanner myInput = new Scanner(System.in);
         System.out.println("kullanıcı isminizi giriniz: ");
         username = myInput.nextLine();
         System.out.println("Şifrenizi giriniz: ");
         password = myInput.nextLine();
-        if (username.equals("ceyhun") && password.equals("dipper46")){
+        if (username.equals("user") && password.equals("1234")){
             System.out.println("giriş başarılı.");
         }
-        else if ((username.equals("ceyhun")) && (!password.equals("dipper46"))){
+        else if ((username.equals("user")) && (!password.equals("1234"))){
             System.out.println("kullanıcı ismi doğru ama şifre hataldıır.");
             System.out.println("şifrenizi değiştirmek ister misiniz ?\nevet için (y)\nhayır için (n)");
             secim = myInput.next();
@@ -21,7 +21,7 @@ public class Main {
                     System.out.println("yeni şifre giriniz: ");
                     new_password = myInput.next();
                     myInput.nextLine();
-                    if (new_password.equals("dipper46")){
+                    if (new_password.equals("1234")){
                         System.out.println("yeni şifreniz eski şifrenizle aynı olamaz!!");
                     }
                     else{
@@ -34,7 +34,7 @@ public class Main {
                     break;
             }
         }
-        else if (!(username.equals("ceyhun")) && (password.equals("dipper46"))){
+        else if (!(username.equals("user")) && (password.equals("1234"))){
             System.out.println("şifre doğru fakat şifre hatalıdır.");
         }
         else {
