2698871d2a0799769d794c959c13514847ec055f
crossgate67 <93184472+crossgate67@users.noreply.github.com>
2022-04-11 03:18:35 +0300

    update

---
+++ src/Aralıkta_asal_sayı_bulduran_program/Main.java
@@ -0,0 +1,19 @@
+package Aralıkta_asal_sayı_bulduran_program;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 2 ; i <= 100 ; i++) {
+            int status = 1;
+            for (int j = 2 ; j < i ; j++) {
+                if (i % j == 0) {
+                    status = 0;
+                }
+            }
+            if (status == 1) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
+
+
