cc3ca335656c34590723668bc19dc80b134a3644
crossgate67 <93184472+crossgate67@users.noreply.github.com>
2022-04-16 14:25:39 +0300

    update

---
+++ src/çok_boyutlu_dizilerle_A_harfi_yazdıran_program/Main.java
@@ -0,0 +1,29 @@
+package çok_boyutlu_dizilerle_A_harfi_yazdıran_program;
+import java.util.Arrays;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        String[][] letter = new String[7][4];
+
+        for (int i = 0; i < letter.length; i++) {
+            for (int j = 0; j < letter[i].length; j++) {
+                if ((i == 0 || i == 3) || i == 6){
+                    letter[i][j] = " * ";
+                }
+                else if (j == 0 || j == 3){
+                    letter[i][j] = " * ";
+                }
+                else{
+                    letter[i][j] = "   ";
+                }
+            }
+        }
+
+        for (String[] row : letter){
+            for (String col : row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
